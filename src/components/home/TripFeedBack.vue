3d6663f21236e4fbd8c9008dbd2f4727527f4559
Eundms <dms011221@gmail.com>
2023-05-15 17:40:06 +0900

    feat: TripFeedBack

--- src/components/home/TripFeedBack.vue
+++ src/components/home/TripFeedBack.vue
@@ -1,37 +1,31 @@
 <template>
-  <v-card class="mx-auto" max-width="50%">
-    <v-list lines="two">
-      <v-list-item
-        prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
-        title="Brunch this weekend?"
-      >
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Ali Connors</span> &mdash; I'll be in your neighborhood
-          doing errands this weekend. Do you want to hang out?
-        </template>
+  <h3 class="d-flex justify-start mt-3">여행 후기</h3>
+
+  <v-list lines="two">
+    <v-card class="mx-auto d-flex justify-center" variant="outlined">
+      <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg">
+        <template v-slot:title> 최고의 여행지였습니다. </template>
+        <template v-slot:subtitle> 여행지 추천 받아서 갔다왔는데 후회없었어요<br /> </template>
+        <!--rating -->
+        <div class="text-center">
+          <v-rating v-model="rating" hover half-increments size="x-small" readonly></v-rating>
+          <pre>{{ rating }}</pre>
+        </div>
       </v-list-item>
+    </v-card>
 
-      <v-divider inset></v-divider>
-
-      <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/lists/2.jpg">
-        <template v-slot:title> Summer BBQ <span class="text-grey-lighten-1">4</span> </template>
-
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">to Alex, Scott, Jennifer</span> &mdash; Wish I could come,
-          but I'm out of town this weekend.
-        </template>
+    <v-card class="mx-auto d-flex justify-center mt-1" variant="outlined">
+      <v-list-item append-avatar="https://cdn.vuetifyjs.com/images/lists/2.jpg">
+        <template v-slot:title> 최고의 여행지였습니다. </template>
+        <template v-slot:subtitle> 여행지 추천 받아서 갔다왔는데 후회없었어요<br /> </template>
+        <!--rating -->
+        <div class="text-center">
+          <v-rating v-model="rating" hover half-increments size="x-small" readonly></v-rating>
+          <pre>{{ rating }}</pre>
+        </div>
       </v-list-item>
-
-      <v-divider inset></v-divider>
-
-      <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/lists/3.jpg" title="Oui oui">
-        <template v-slot:subtitle>
-          <span class="font-weight-bold">Sandra Adams</span> &mdash; Do you have Paris
-          recommendations? Have you ever been?
-        </template>
-      </v-list-item>
-    </v-list>
-  </v-card>
+    </v-card>
+  </v-list>
 </template>
 
 <script>
